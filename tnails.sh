6bdef5a0b0f293c8cef3f3653e494c4aa840eb62
Yonn Lopez <4582800-yonnlopez@users.noreply.gitlab.com>
2023-05-16 16:29:48 +0000

    Update tnails.sh

--- tnails.sh
+++ tnails.sh
@@ -1,14 +1,14 @@
 ffmpeg-thumbart () {
-FILES=(*.mkv)
-
-for i in "${FILES[@]}"; do
+FILES=(alarm*.mkv)
+IMAGES=(*.png)
+for i in "${!FILES[@]}"; do
 IMAGE=$((i+1))
 
-input=video_$IMAGE.mkv
+input=$FILES
 
-ffmpeg -i $input -i cover_$IMAGE.png -c copy -map 0 -map 1 tnail-$(date +"%d-%b-%Y_%H.%M.%S%p")_$IMAGE.mkv
+ffmpeg -i $input -i $IMAGES -c copy -map 0 -map 1 tnail-$(date +"%d-%b-%Y_%H.%M.%S%p")_$IMAGE.mkv
 done
 
 }
 
-#notes, the png file must have a name: cover_1.png in increments, same applyies for the video filename. all files must be in the same directory for this bash script to work, find and join the png file to the mkv video_poscast. extensions and filenames matter in this case. (cover_"$1") & (video_"$1") are static the numbering will changue on the output file but its real on the dir files. You can alo add this into your .bashrc file.  
+#notes, the png file is ok with *.png, extension, the video filename will have to start with alarm*.mkv for this script. all files must be in the same directory for this bash script to work, find and join the png file to the mkv video. extensions and filenames matter in this case. the numbering will changue on the output file. ***You can alo add this into your .bashrc file. ***
